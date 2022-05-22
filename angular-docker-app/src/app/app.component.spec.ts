7616060d31ada0b4862d4c67209cc91b605c8bb1
azad ansari <azadansari7277@gmail.com>
2022-05-22 17:16:34 +0530

    changes done

--- angular-docker-app/src/app/app.component.spec.ts
+++ angular-docker-app/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'docProjectforphsaefour'`, () => {
+  it(`should have as title 'angular-docker-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('docProjectforphsaefour');
+    expect(app.title).toEqual('angular-docker-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('docProjectforphsaefour app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-docker-app app is running!');
   });
 });
